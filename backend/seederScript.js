2b4b567d23e8bc9694da81ad7f9b545aa622bcc3
Rishitha Ramesh <rishitha8112000@gmail.com>
2021-05-03 20:19:45 +0530

    First commit

---
+++ backend/seederScript.js
@@ -0,0 +1,24 @@
+// we use the model we created in models/Product.js
+require("dotenv").config();
+
+const productData = require("./data/products");
+const connectDB = require("./config/db");
+const Product = require("./models/Product");
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await Product.deleteMany({}); // delete everything in current db
+    await Product.insertMany(productData); //insert evrything in data/products.js- line 4
+
+    console.log("Data Import Success");
+
+    process.exit();
+  } catch (error) {
+    console.error("Error with data import", error);
+    process.exit(1);
+  }
+};
+
+importData();
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config(); // so we have access to env
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json()); //to create routes.
+
+app.get("/", (req, res) => {
+    res.json({ message: "API running..." });
+  });  
+
+app.use("/api/products", productRoutes); //api/products will have access to productRoutes
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
