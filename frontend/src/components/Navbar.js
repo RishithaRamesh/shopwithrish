ceeb226a744dab98fc8b4b74ecb522d174485dfd
Rishitha Ramesh <rishitha8112000@gmail.com>
2021-05-04 18:19:57 +0530

    navbar ready

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,16 +13,17 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
-        <h2>MERN Shopping Cart</h2>
+        <h2>Shoppee</h2>
       </div>
 
       <ul className="navbar__links">
         <li>
-          <Link to="/cart" >
+          <Link to="/cart" className="cart__link" >
             <i className="fas fa-shopping-cart"></i>
-            {/* <span> */}
-              Cart <span className="cartlogo__badge">0</span>
-            {/* </span> */}
+            <span>
+              Cart 
+              <span className="cartlogo__badge">0</span>
+            </span>
           </Link>
         </li>
         <li>
