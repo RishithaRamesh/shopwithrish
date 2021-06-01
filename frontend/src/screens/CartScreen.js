39155c7007b63befb228b4a908da58b07da03d81
Rishitha Ramesh <rishitha8112000@gmail.com>
2021-06-01 19:39:52 +0530

    .

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -39,13 +39,13 @@ const CartScreen = () => {
     <>
       <div className="cartscreen">
         <div className="cartscreen__left">
-          <h2>Shopping Cart</h2>
+          <h1 className="proceed">Your Cart</h1>
 
           {cartItems.length === 0 ? (
             <div>
               Your Cart Is Empty <Link to="/">Go Back</Link>
             </div>
-          ) : ( 
+          ) : (
             // if cart is not empty: we loop through each item in the cart and render it 
             // i.e call <CartItem /> with arguments
             cartItems.map((item) => (
@@ -61,10 +61,20 @@ const CartScreen = () => {
 
         <div className="cartscreen__right">
           <div className="cartscreen__info">
-            <p>Subtotal ({getCartCount()}) items</p>
-            <p>${getCartSubTotal()}</p>
+            {/* <p>Subtotal ({getCartCount()}) items</p> */}
+            <h2>ORDER SUMMARY</h2>
+
+            <div className="ordersummary">
+              <p>ITEMS</p>
+              <p className="push">{getCartCount()}</p>
+            </div>
+            <div className="ordersummary">
+              <p>TOTAL COST</p>
+              <p className="push">${getCartSubTotal()}</p>
+            </div>
+            
           </div>
-          <div>
+          <div className="proceed">
             <button>Proceed To Checkout</button>
           </div>
         </div>
