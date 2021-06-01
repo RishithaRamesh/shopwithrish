677e8bceb26b22bb9347ec6f8d89d22fe6a9f8ff
Rishitha Ramesh <rishitha8112000@gmail.com>
2021-06-01 18:24:29 +0530

    .

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -22,8 +22,10 @@ function bgChanger() {
   console.log(this.scrollY);
   if(this.scrollY > this.innerHeight / 2){
     document.body.classList.add('bg-active');
+    // document.body.classList.add('homescreen__title');
   }else{
     document.body.classList.remove("bg-active");
+    // document.body.classList.remove('homescreen__title');
   }
 }
 window.addEventListener("scroll", bgChanger);
